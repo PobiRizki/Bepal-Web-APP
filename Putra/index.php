7188387f6e6480fccc895765296c60eb0d3c056d
Putra Arifah <putraarifah@gmail.com>
2018-02-05 13:30:49 +0700

    Halaman Awal
    Deskripsi pribadi

---
+++ Putra/index.php
@@ -0,0 +1,15 @@
+<htm>
+<head>
+    <title>PUTRA ARIFAH</title>
+</head>
+
+<body>
+    <center><h1>Masih Dalam Tahap Pengembangan</h1></center>
+    <center><p>Info Menarik Lainnya Tentang <b><a href="https://dafunda.com/anggota/putra-arifah/">Putra Arifah</a></b></p></center>
+    <center><p>Cek Media Sosialnya</p></center>
+    <center><a href="https://www.instagram.com/putraarifah/"><img src="https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=w300" style="width: 30px; height: 30px;"></a>
+    <a href="https://www.youtube.com/channel/UC49xYqEOpF24cKN-WEeIf2Q"><img src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697037-youtube-512.png" style="width: 30px; height: 30px;"></a>
+    <a href="https://www.facebook.com/putra.arifah14"><img src="https://images.sftcdn.net/images/t_optimized,f_auto/p/8a6eafba-96d2-11e6-b334-00163ec9f5fa/77332936/facebook-logo.png" style="width: 30px; height: 30px;"></a></center>
+</body>    
+    
+</htm>
